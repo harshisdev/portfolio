4cb87e3d670fc1a9e21dae022d06f2f86015b0f9
Harsh Kumarhttps://avatars.githubusercontent.com/u/106384346?s=96&v=4 <106384346+harshisdev@users.noreply.github.com>
2024-04-09 16:59:23 +0530

    Update Loader.js
    LOADER CHANGE

--- src/assets/component/Loader.js
+++ src/assets/component/Loader.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { ColorRing } from 'react-loader-spinner';
+import { ThreeDots } from 'react-loader-spinner';
 
 const Loader = () => {
     return (
         <>
             <div className="w-100 vh-100 d-flex justify-content-center align-items-center">
-                <ColorRing
+                <ThreeDots
                     visible={true}
                     height="60"
                     width="60"
-                    ariaLabel="color-ring-loading"
+                     ariaLabel="three-dots-loading"
                     wrapperStyle={{}}
-                    wrapperClass="color-ring-wrapper"
-                    colors={['#E70448', '#FBBC04', '#4285F4', '#34A853', '#2C3E50']}
+                    wrapperClass="three-dots-loading"
+                    color="#ff014f"
                 />
             </div>
         </>

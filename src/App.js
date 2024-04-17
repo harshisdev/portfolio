7eb620d68be7f434144ddea93f3820ccc2f6192f
Harsh Kumarhttps://avatars.githubusercontent.com/u/106384346?s=96&v=4 <106384346+harshisdev@users.noreply.github.com>
2024-04-17 10:56:42 +0530

    Update App.js
    analytics update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 import Home from "./pages/Home";
 import Footer from "./component/Footer";
 import AboutPages from "./pages/AboutPages";
@@ -54,6 +55,7 @@ function App() {
                   <Route path="*" element={<Erorr />} />
                 </Routes>
                 <Footer />
+                <Analytics />
               </>
             )
         }

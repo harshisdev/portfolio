1639fb9ea92fbedd75501e42a9033efc1cccb3e1
Harsh Kumar <harsh.kumar@myhealthcare.life>
2024-10-27 21:58:06 +0530

    fix ui

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -20,7 +20,7 @@ const Banner = () => {
             />
           </span>
           <br />
-          <span style={{lineHeight:'2'}}>A Dedicated Frontend Web Developer with <b>2 years 3 months</b> of hands-on experience in creating intuitive and visually appealing user interfaces. <br />Adept at translating design concepts into responsive and user-friendly web applications. Experienced in optimizing websites for search engines and implementing efficient web performance techniques. <br />Strong collaborative skills demonstrated through successful cross-functional teamwork with product and marketing teams. I stay current with evolving technologies and I am committed to continuous learning and growth.</span>
+          <span style={{lineHeight:'2'}}>A Dedicated Frontend Web Developer with <b className="text-primary">2 years 3 months</b> of hands-on experience in creating intuitive and visually appealing user interfaces. <br />Adept at translating design concepts into responsive and user-friendly web applications. Experienced in optimizing websites for search engines and implementing efficient web performance techniques. <br />Strong collaborative skills demonstrated through successful cross-functional teamwork with product and marketing teams. I stay current with evolving technologies and I am committed to continuous learning and growth.</span>
         </div>
         <div className="col-3 justify-content-end d-flex">
           <img data-aos="fade-left" src={harshImages} style={{ width: 'auto', height: '280px', boxShadow: 'rgba(57, 76, 96, 0.15) 0px 0px 10px 13px' }} alt="Harsh Kumar" />
